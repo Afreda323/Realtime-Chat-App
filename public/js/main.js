39c0fef94a6ca3cf820709e9991987d7086d45ba
Afreda323 <anthonyfreda323@gmail.com>
2017-06-23 10:53:31 -0400

    brodacasting connect message

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,6 @@
   let socket = io();
   socket.on("connect", function() {
     console.log("Connected");
-    socket.emit("createMessage", {
-      from: "Sloo",
-      text: "fuk off mate"
-    });
   });
   socket.on("disconnect", function() {
     console.log("Disconnected from server");
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+
 const socketIO = require("socket.io");
 
 const public = path.join(__dirname, "../public");
@@ -13,23 +14,33 @@ app.use(express.static(public));
 
 io.on("connection", socket => {
   console.log("New connection");
-  socket.emit('newMessage', {
-      from: 'Jimmy',
-      text: 'Ayy lamo',
+  socket.emit("newMessage", {
+    from: "Admin",
+    text: "Welcome to the chat app",
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit("newMessage", {
+    from: "Admin",
+    text: "New person joined",
+    createdAt: new Date().getTime()
+  });
+  socket.on("createMessage", message => {
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
       createdAt: new Date().getTime()
-  })
-  socket.on('createMessage', (message) => {
-      io.emit('newMessage', {
-          from: message.from,
-          text: message.text,
-          createdAt: new Date().getTime()
-      })
-  })
+    });
+  });
   socket.on("disconnect", () => {
     console.log("New Disconnect");
   });
 });
 
+//Live reload on local
+if (port === 3000) {
+  const reload = require("reload");
+  reload(server, app);
+}
 
 server.listen(port, () => {
   console.log("Up on " + port);
