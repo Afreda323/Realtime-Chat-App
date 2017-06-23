01782fb25db545dd1e9e378ed896114e2088b2a1
Afreda323 <anthonyfreda323@gmail.com>
2017-06-23 13:55:27 -0400

    Templates for messages

--- public/js/index.js
+++ public/js/index.js
@@ -10,18 +10,26 @@ socket.on("disconnect", function() {
 });
 
 socket.on("newMessage", function(message) {
-  let li = $("<li></li>");
-  li.text(`${message.from}: ${message.text}`);
-  $("#messages").append(li);
+  const time = moment(message.createdAt).format("h:mm a");
+  const template = $("#message-template").html();
+  const html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    time: time
+  });
+
+  $("#messages").append(html);
 });
 
 socket.on("newLocationMessage", function(message) {
-  let li = $("<li></li>");
-  let a = $('<a target="_blank">My Current Location</a>');
-  li.text(`${message.from}: `);
-  a.attr("href", message.url);
-  li.append(a);
-  $("#messages").append(li);
+  const time = moment(message.createdAt).format("h:mm a");
+  const template = $('#location-message-template').html();
+  const html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    time: time
+  })
+  $('#messages').append(html)
 });
 
 $("#message-form").on("submit", function(e) {
@@ -42,21 +50,21 @@ $("#message-form").on("submit", function(e) {
 
 locateButton.on("click", function(e) {
   e.preventDefault();
-  
+
   if (!navigator.geolocation) {
     return alert("Geolocation not supported");
   }
-  locateButton.attr('disabled', 'disabled').text('Sending Location...');
+  locateButton.attr("disabled", "disabled").text("Sending Location...");
   navigator.geolocation.getCurrentPosition(
     function(position) {
-      locateButton.removeAttr('disabled').text('Send Location');
+      locateButton.removeAttr("disabled").text("Send Location");
       socket.emit("createLocationMessage", {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
       });
     },
     function() {
-      locateButton.removeAttr('disabled').text('Send Location');
+      locateButton.removeAttr("disabled").text("Send Location");
       alert("Unable to fetch location");
     }
   );
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,17 +1,17 @@
 const GOOGLE_URL = "https://www.google.com/maps?q=";
-
+const moment = require('moment')
 const generateMessage = (from, text) => {
     return {
         from,
         text,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 }
 const generateLocationMessage = (from, lat, lon) => {
     return {
         from,
         url: `${GOOGLE_URL}${lat},${lon}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     }
 }
 module.exports = {
